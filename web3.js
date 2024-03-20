941dc8198d6fc21ce4075617a29f3ba10672eecd
hieultgch200617 <hieultgch200617@fpt.edu.vn>
2024-03-20 17:22:16 +0700

    first commit

---
+++ web3.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer((req, res) => {
+    if (req.url ==='/')
+    {
+        fs.readFile('index.html', (err, data) => {
+            if (!err) {
+                res.write(data);
+                res.end;
+            }
+        });
+    } else if (req.url === '/greenwich'){
+        fs.readFile('greenwich.html', (err, data) => {
+        if (!err) {
+            res.write(data);
+            res.end;
+        }
+    });
+    } else if (req.url === '/fpt'){
+        fs.readFile('fpt.html', (err, data) => {
+            if (!err) {
+                res.write(data);
+                res.end;
+            }
+        });
+    } else {
+        res.end("<h1>404 - Not Found </h1>")
+    }
+})
+
+server.listen(8888)
